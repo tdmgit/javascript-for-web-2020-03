cafb11d3feb5cc6c6680ca94164fe91d69599298
Teresa Holfeld <teresa.holfeld@gmail.com>
2020-02-28 15:02:35 +0100

    json in js

---
+++ micro-blog/index.js
@@ -0,0 +1,12 @@
+let post = {
+  title: "In The Mountains",
+  image: "img/card-top.jpg",
+  image_alt: "Sunset in the mountains",
+  text:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  author: "Firstname Lastname",
+  author_image: "img/jonathan.jpg",
+  date: "Jan 18, 2020",
+  number: 1
+};
+console.log(post);
